2b1cbe948d1cab09df500e4212ae97b4a7d04301
karan.verma <karan.verma@skoov.com>
2016-07-02 14:26:04 +0530

    adding new controller

--- app.js
+++ app.js
@@ -1,69 +1,69 @@
 (function() {
     var app = angular.module('music-app', ['ngMaterial', 'ngResource', 'ngRoute']);
 
-    app.controller('musicAppController', ['$resource', function($resource) {
-        var musicApp = this;
-        musicApp.showNewTrack = false;
-        musicApp.showEditTrack = false;
+    app.controller('trackController', ['$resource', function($resource) {
+        var track = this;
+        track.showNewTrack = false;
+        track.showEditTrack = false;
         
         this.getResource = function(url) {
             return $resource(url);
         }
         this.showNewTrackForm = function(){
-            musicApp.showNewTrack = true;
+            track.showNewTrack = true;
             console.log('show new track from button clicked');
         }
         this.hideNewTrackForm = function(){
-            musicApp.showNewTrack = false;
+            track.showNewTrack = false;
             console.log('hide new track from button clicked');   
         }
         this.getId = function(genID){
-            musicApp.genres = genID;
+            track.genres = genID;
         }
         this.saveNewTrack = function(){
-            musicApp.showNewTrack = false;
+            track.showNewTrack = false;
             var url = 'http://104.197.128.152:8000/v1/tracks';
-            musicAppFactory = this.getResource(url);
+            trackFactory = this.getResource(url);
              // <<<<< This is how do you write in track >>>>>
-            newsong = new musicAppFactory();
-            newsong.data = JSON.stringify({title : musicApp.trackname, rating : musicApp.rating, genres: [musicApp.genres]});
+            newsong = new trackFactory();
+            newsong.data = JSON.stringify({title : track.trackname, rating : track.rating, genres: [track.genres]});
             console.log(newsong.data);
             // CHECK OUT! : bit of a confusion here regarding using newsong as a factory
-            // where as if assigned to normal object musicAppFactory. save will work perfectly.
-            musicAppFactory.save(newsong.data, function() {
+            // where as if assigned to normal object trackFactory. save will work perfectly.
+            trackFactory.save(newsong.data, function() {
                 console.log("you are awesome!");
             });
 
         }
         this.search = function(){
             var url = 'http://104.197.128.152:8000/v1/tracks';
-            musicAppFactory = this.getResource(url);
+            trackFactory = this.getResource(url);
             // // <<<<< This is how do you search track given its title name >>>>>
-            var entry = musicAppFactory.get({ title:musicApp.searchKeyword }, function() {
+            var entry = trackFactory.get({ title:track.searchKeyword }, function() {
                 console.log(entry);
-                musicApp.tracks = entry.results;
+                track.tracks = entry.results;
             }); 
 
         }
         this.showEditTrackForm = function(title,rating,genres,id){
-            musicApp.showEditTrack =true;
-            musicApp.trackname = title;
-            musicApp.rating = rating;
-            musicApp.id = id;
-            musicApp.genres = genres;
+            track.showEditTrack =true;
+            track.trackname = title;
+            track.rating = rating;
+            track.id = id;
+            track.genres = genres;
         }
 
         this.hideEditTrackForm = function(){
-            musicApp.showEditTrack =false;
+            track.showEditTrack =false;
         }
         this.updateTrack = function(title,rating,genres,id){
 
-            editsong = new musicAppFactory();
+            editsong = new trackFactory();
             editsong.data = JSON.stringify({id : id, title : title, rating : rating, genres : [genres]});
             var url = 'http://104.197.128.152:8000/v1/tracks/'+id;
-            musicAppFactory = this.getResource(url);
+            trackFactory = this.getResource(url);
             console.log("this is what is been sent => "+editsong.data);
-            musicAppFactory.save(editsong.data, function() {
+            trackFactory.save(editsong.data, function() {
                 console.log("you are reallllly awesome!");
             });
         }
@@ -71,68 +71,68 @@
         this.loadGenres =  function(){
             console.log("button is clicked!");
             var url = 'http://104.197.128.152:8000/v1/genres';
-            musicAppFactory = this.getResource(url);
-            var val = musicAppFactory.get(function(){
+            trackFactory = this.getResource(url);
+            var val = trackFactory.get(function(){
                 console.log(val);
-                musicApp.allGenres = val.results;
-                musicApp.genresNext = val.next;
+                track.allGenres = val.results;
+                track.genresNext = val.next;
             });
         }
         this.loadMore = function(old_gen){
             console.log("load more button is clicked!");
-            var url = musicApp.genresNext;
-            console.log("next genres url => "+musicApp.genresNext);
-            musicAppFactory = this.getResource(url);
-            var val = musicAppFactory.get(function(){
+            var url = track.genresNext;
+            console.log("next genres url => "+track.genresNext);
+            trackFactory = this.getResource(url);
+            var val = trackFactory.get(function(){
                 console.log("load more is successful!");
-                musicApp.allGenres = old_gen;
+                track.allGenres = old_gen;
                 console.log(typeof(old_gen));
                 console.log(old_gen);
-                musicApp.allGenres.push.apply(musicApp.allGenres,val.results);
-                // musicApp.allGenres = val.results;
+                track.allGenres.push.apply(track.allGenres,val.results);
+                // track.allGenres = val.results;
 //                 var x = ['a', 'b', 'c'];
 // var y = ['d', 'e', 'f'];
 // x.push.apply(x, y);
-                musicApp.genresNext = val.next;
+                track.genresNext = val.next;
             });            
         }
 
         this.allsongs = function() {
             var url = 'http://104.197.128.152:8000/v1/tracks/:id';
-            musicAppFactory = this.getResource(url);
+            trackFactory = this.getResource(url);
             // var val1 = "none";
 
             // // <<<<< This is how do you get one track from api >>>>>
-            // var entry = musicAppFactory.get({ id: 27 }, function() {
+            // var entry = trackFactory.get({ id: 27 }, function() {
             //     console.log(entry);
             // }); // get() returns a single entry
 
 
             // <<<<< This is how do you show all data from track >>>>>
 
-            var val = musicAppFactory.get(
+            var val = trackFactory.get(
                 function() {
                     console.log("Val value is => ", val.results);
                     response = val;
-                    musicApp.tracks = val.results;
-                    musicApp.next = val.next;
-                    musicApp.prev = val.previous;
+                    track.tracks = val.results;
+                    track.next = val.next;
+                    track.prev = val.previous;
                     console.log(val.next);
                 });
 
             // // <<<<< This is how do you write in track >>>>>
-            // newsong = new musicAppFactory();
+            // newsong = new trackFactory();
             // newsong.data = JSON.stringify({title : "the minion song99", rating : 5, genres: [29]});
             // console.log(newsong.data);
             // // CHECK OUT! : bit of a confusion here regarding using newsong as a factory
-            // // where as if assigned to normal object musicAppFactory. save will work perfectly.
-            // musicAppFactory.save(newsong.data, function() {
+            // // where as if assigned to normal object trackFactory. save will work perfectly.
+            // trackFactory.save(newsong.data, function() {
             //     console.log("you are awesome!");
             // });
 
             // 
             // // <<<<< This is how do you search track given its title name >>>>>
-            // var entry = musicAppFactory.get({ title:'the minion song99' }, function() {
+            // var entry = trackFactory.get({ title:'the minion song99' }, function() {
             //     console.log(entry);
             // }); 
 
@@ -140,34 +140,34 @@
         }
 
         this.nextTrack = function() {
-            var url = musicApp.next;
-            musicAppFactory = this.getResource(url);
-            var val = musicAppFactory.get(
+            var url = track.next;
+            trackFactory = this.getResource(url);
+            var val = trackFactory.get(
                 function() {
                     console.log("Val value is => ", val.results);
-                    musicApp.tracks = val.results;
-                    musicApp.next = val.next;
-                    musicApp.prev = val.previous;
+                    track.tracks = val.results;
+                    track.next = val.next;
+                    track.prev = val.previous;
                 });
         }
         this.prevTrack = function() {
-            var url = musicApp.prev;
-            musicAppFactory = this.getResource(url);
-            var val = musicAppFactory.get(
+            var url = track.prev;
+            trackFactory = this.getResource(url);
+            var val = trackFactory.get(
                 function() {
                     console.log("Val value is => ", val.results);
-                    musicApp.tracks = val.results;
-                    musicApp.next = val.next;
-                    musicApp.prev = val.previous;
+                    track.tracks = val.results;
+                    track.next = val.next;
+                    track.prev = val.previous;
                 });
         }
         this.addNewTrack = function() {
             var url = 'http://104.197.128.152:8000/v1/tracks/:id';
-            musicAppFactory = this.getResource(url);
-            newsong = new musicAppFactory();
+            trackFactory = this.getResource(url);
+            newsong = new trackFactory();
             newsong.data = JSON.stringify({ title: "the minion song990", rating: 5, genres: [29] });
             console.log(newsong.data);
-            musicAppFactory.save(newsong.data, function() {
+            trackFactory.save(newsong.data, function() {
                 console.log("you are awesome!");
             });
 
@@ -180,6 +180,9 @@
 
 
     }]); //closing controller
+
+
+
     app.config(function($mdThemingProvider) {
         $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
         $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
